fe510a9875a2835f36c42ab1af22fd7b32e3bd41
Tiberiu Patrascu <you@example.com>
2018-01-05 03:33:46 +0200

    cache metrics

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 
 import os
 import math
+import time
+import threading
 import urllib2
 import SimpleHTTPServer
 import SocketServer
@@ -11,18 +13,46 @@ from openshift import client, config
 from prometheus_client.parser import text_string_to_metric_families
 
 
-metric_cache = {}
+metrics_cache = {}
+
 
 def scrape_metrics():
-    request_url = '{}://{}/federate?match[]={{job="{}"}}'.format(scheme, upstream, prometheus_scrape_job)
-    if debug:
-        print('Requesting {}'.format(request_url))
-    response = urllib2.urlopen(request_url)
-    prometheus_text_response = response.read()
+    while True:
+        t0 = time.time()
+        if debug:
+            print('Start scraping {}'.format(request_url))
+        
+        prometheus_text_response = ''
+        try:
+            prometheus_text_response = urllib2.urlopen(request_url).read()
+        except Exception as e:
+            if debug: print(e)
 
-    new_metrics = {}
-    for family in text_string_to_metric_families(prometheus_text_response):
-        for sample in family.samples:
+        new_metrics = {}
+        for family in text_string_to_metric_families(prometheus_text_response):
+            for sample in family.samples:
+                if 'namespace' in sample[1]:
+                    if sample[1]['namespace'] not in new_metrics:
+                        new_metrics[sample[1]['namespace']] = {
+                            family.name: {
+                                'type': family.type,
+                                'documentation': family.documentation,
+                                'samples': [sample],
+                            }
+                        }
+                    else:
+                        new_metrics[sample[1]['namespace']][family.name]['samples'].append(sample)
+        
+        metrics_cache = new_metrics
+
+        t1 = time.time()
+        total_time = t1 - t0
+
+        if debug:
+            print('End scraping. duration: {} seconds'.format(total_time))
+        
+        if total_time < scrape_interval_sec:
+            time.sleep(scrape_interval_sec - total_time)
 
 
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -46,28 +76,26 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                     print("Listing user projects")
                 oapi = client.OapiApi(client.ApiClient(header_name='Impersonate-User', header_value=oauth_user))
                 user_projects = [x.metadata.name for x in oapi.list_project().items]
-
-                if debug:
-                    print("Filtering and outputing metrics")
-                for family in text_string_to_metric_families(prometheus_text_response):
-                    found_samples = []
-                    for sample in family.samples:
-                        if 'namespace' in sample[1] and sample[1]['namespace'] in user_projects:
-                            found_samples.append(sample)
-                    
-                    if found_samples:
-                        body += '# HELP {} {}\n'.format(family.name, family.documentation)
-                        body += '# TYPE {} {}\n'.format(family.name, family.type)
-                    
-                    for sample in found_samples:
-                        sample_metric_name = sample[0]
-                        sample_value = sample[2]
-                        if isinstance(sample_value, float) and math.isnan(sample_value):
-                            sample_value = 'NaN'
-                        sample_prom_labels = ','.join(['{}="{}"'.format(x[0], x[1]) for x in sample[1].iteritems()])
-                        body += '{0} {{{1}}} {2}\n'.format(sample_metric_name, sample_prom_labels, sample_value)
             except:
+                response_code = 500
+                body = 'Failed to list user projects.\n'
                 if debug: raise
+            
+            if debug:
+                print("Outputing metrics")
+            
+            for project in user_projects:
+                for name, attrs  in metrics_cache[project].iteritems():
+                    body += '# HELP {} {}\n'.format(name, attrs.documentation)
+                    body += '# TYPE {} {}\n'.format(name, attrs.type)
+                
+                for sample in attrs.samples:
+                    sample_metric_name = sample[0]
+                    sample_value = sample[2]
+                    if isinstance(sample_value, float) and math.isnan(sample_value):
+                        sample_value = 'NaN'
+                    sample_prom_labels = ','.join(['{}="{}"'.format(x[0], x[1]) for x in sample[1].iteritems()])
+                    body += '{0} {{{1}}} {2}\n'.format(sample_metric_name, sample_prom_labels, sample_value)
         else:
             response_code = 403
             body = 'Authentication error.\n'
@@ -92,11 +120,17 @@ if __name__ == "__main__":
     upstream = os.environ.get('PROMETHEUS_UPSTREAM_TARGET', 'prometheus:9090')
     scheme = os.environ.get('PROMETHEUS_UPSTREAM_SCHEME', 'http')
     prometheus_scrape_job = os.environ.get('PROMETHEUS_SCRAPE_JOB', 'kubernetes-cadvisor')
-    scrape_freq_sec = os.environ.get('SCRAPE_FREQ_SEC', '60')
+    scrape_interval_sec = int(os.environ.get('SCRAPE_INTERVAL_SEC', '60'))
+    request_url = '{}://{}/federate?match[]={{job="{}"}}'.format(scheme, upstream, prometheus_scrape_job)
     
     debug = False
     if 'DEBUG' in os.environ and os.environ['DEBUG'] in ('True', '1'):
         debug = True
+
+    print('Starting metrics scraping thread')
+    thread = threading.Thread(target=scrape_metrics)
+    thread.daemon = True
+    thread.start()
 
     print('Server listening on port 8080...')
     print('Debug', debug)
