8f7fde6046839a2bae3bae3a5bfd0b85ab73cc78
Tiberiu Patrascu <you@example.com>
2018-01-07 01:12:19 +0200

    simplify...

--- app.py
+++ app.py
@@ -3,57 +3,12 @@
 
 import os
 import math
-import time
-import threading
 import urllib2
 import SimpleHTTPServer
 import SocketServer
 
 from openshift import client, config
 from prometheus_client.parser import text_string_to_metric_families
-
-
-metrics_cache = {}
-
-def scrape_metrics():
-    global metrics_cache
-
-    while True:
-        t0 = time.time()
-        if debug:
-            print('Start scraping {}'.format(request_url))
-        
-        try:
-            prometheus_text_response = urllib2.urlopen(request_url).read()
-
-            new_metrics = {}
-            for family in text_string_to_metric_families(prometheus_text_response):
-                for sample in family.samples:
-                    if 'namespace' in sample[1]:
-                        if sample[1]['namespace'] not in new_metrics:
-                            new_metrics[sample[1]['namespace']] = {}
-                        
-                        if family.name not in new_metrics[sample[1]['namespace']]:
-                            new_metrics[sample[1]['namespace']][family.name] = {
-                                'type': family.type,
-                                'documentation': family.documentation,
-                                'samples': [sample],
-                            }
-                        else:
-                            new_metrics[sample[1]['namespace']][family.name]['samples'].append(sample)
-        except Exception as e:
-            if debug: print(e)
-        
-        metrics_cache = new_metrics
-
-        t1 = time.time()
-        total_time = t1 - t0
-        
-        if debug:
-            print('End scraping. duration: {} seconds'.format(total_time))
-        
-        if total_time < scrape_interval_sec:
-            time.sleep(scrape_interval_sec - total_time)
 
 
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -80,24 +35,19 @@ class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             except:
                 response_code = 500
                 body = 'Failed to list user projects.\n'
-                if debug: raise
             
             if debug:
                 print("Outputing metrics")
-            
+
             for project in user_projects:
-                if project in metrics_cache:
-                    for metric_family_name, attrs  in metrics_cache[project].iteritems():
-                        body += '# HELP {} {}\n'.format(metric_family_name, attrs['documentation'])
-                        body += '# TYPE {} {}\n'.format(metric_family_name, attrs['type'])
-                    
-                        for sample in attrs['samples']:
-                            sample_metric_name = sample[0]
-                            sample_value = sample[2]
-                            if isinstance(sample_value, float) and math.isnan(sample_value):
-                                sample_value = 'NaN'
-                            sample_prom_labels = ','.join(['{}="{}"'.format(x[0], x[1]) for x in sample[1].iteritems()])
-                            body += '{0} {{{1}}} {2}\n'.format(sample_metric_name, sample_prom_labels, sample_value)
+                for prometheus_job in prometheus_jobs:
+                    request_url = '{0}://{1}/federate?match[]={{job="{2}",namespace="{3}"}}'.format(
+                                    scheme, upstream, prometheus_job, project)
+                    try:
+                        body += urllib2.urlopen(request_url).read()
+                    except:
+                        response_code = 500
+                        body = 'Failed to get metrics.\n'
         else:
             response_code = 403
             body = 'Authentication error.\n'
@@ -121,19 +71,11 @@ if __name__ == "__main__":
     
     upstream = os.environ.get('PROMETHEUS_UPSTREAM_TARGET', 'prometheus:9090')
     scheme = os.environ.get('PROMETHEUS_UPSTREAM_SCHEME', 'http')
-    prometheus_scrape_job = os.environ.get('PROMETHEUS_SCRAPE_JOB', 'kubernetes-cadvisor')
-    scrape_interval_sec = int(os.environ.get('SCRAPE_INTERVAL_SEC', '60'))
-    request_url = '{}://{}/federate?match[]={{job="{}"}}'.format(scheme, upstream, prometheus_scrape_job)
+    prometheus_jobs = os.environ.get('PROMETHEUS_JOBS', 'kubernetes-cadvisor').split(' ')
     
     debug = False
     if 'DEBUG' in os.environ and os.environ['DEBUG'] in ('True', '1'):
         debug = True
-
-    print('Starting metrics scraping thread')
-    thread = threading.Thread(target=scrape_metrics)
-    thread.daemon = True
-    thread.start()
-    time.sleep(2)
 
     print('Server listening on port 8080...')
     print('Debug', debug)
